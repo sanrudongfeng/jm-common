4a25af07e1ebcae24eef89134bc8a725564ef69c
jeffyujm <2651339@qq.com>
2014-08-12 21:35:12 +0800

    0.0.3
    support multi db connection
    SequenceDao as Class
    add test.js

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 var lib = require('./');
 var MQ = lib.MQ;
+var db = lib.db;
+var SequenceDao = lib.SequenceDao;
 
-var test = function(req,res){
+function testDB(){
+    //db1
+    var dbUri = 'mongodb://localhost/test';
+    var c = db.connect(dbUri, true);
+    var sd = new SequenceDao(c);
+    sd.getNext('uid', function(err, v){
+        console.info(v);
+        db.disConnect(dbUri);
+    });
+
+    //db2 default DB
+    db.connect();
+    sd = new SequenceDao();
+    sd.getNext('uid', function(err, v){
+        console.info(v);
+        db.disConnect();
+    });
+};
+
+function test(){
+    testDB();
 };
 
 test();
