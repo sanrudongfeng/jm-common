2fbe3a20484e7587509aec1584db83ca55d7ef2f
jeffyujm <2651339@qq.com>
2014-08-22 17:44:03 +0800

    0.0.8
    optimize

--- test.js
+++ test.js
@@ -1,35 +1,42 @@
 var lib = require('./');
 var MQ = lib.MQ;
-var db = lib.db;
+var DB = lib.DB;
 var SequenceDao = lib.SequenceDao;
 
-function testDB(){
+(function testDB(){
     //db1
     var dbUri = 'mongodb://localhost/test';
-    var c = db.connect(dbUri, true);
-    var c1= db.connect(dbUri, true);
-    var c2= db.connect(dbUri, true);
-    db.disConnect(dbUri);
-    db.disConnect(dbUri);
+    var c = DB.connect(dbUri, true);
+    var c1= DB.connect(dbUri, true);
+    var c2= DB.connect(dbUri, true);
+    DB.disConnect(dbUri);
+    DB.disConnect(dbUri);
     var sd = new SequenceDao(c);
     sd.getNext('uid', function(err, v){
         console.info(v);
-        db.disConnect(dbUri);
+        DB.disConnect(dbUri);
     });
 
     //db2 default DB
-    db.connect();
+    DB.connect();
     sd = new SequenceDao();
     sd.getNext('uid', function(err, v){
         console.info(v);
-        db.disConnect();
+        DB.disConnect();
     });
-};
+})();
 
-function test(){
-    testDB();
-};
+//testMQ
+(function testMQ(){
+    var mq = new MQ();
+    mq.set('account', 'test', function(err){
+        if(err){
+            console.info(err);
+            return;
+        }
+        mq.get('account', function(err, v){
+            console.info(v);
+        });
+    });
 
-test();
-
-
+})();
